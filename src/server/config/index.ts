44b93350374099c6bac5910e3f8b98704c76ca84
skcross <souren@tradable.live>
2024-03-18 17:57:00 -0700

    first commit

---
+++ src/server/config/index.ts
@@ -0,0 +1,12 @@
+import { createEmitAndSemanticDiagnosticsBuilderProgram } from "typescript";
+
+export default {
+    mysql: {
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT,
+        user: process.env.DB_USER,
+        database: process.env.DB_SCHEMA,
+        password: process.env.DB_PASS
+
+    }
+}
